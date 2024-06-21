8858dec48bd064785db878614d3f479e639d9e30
DavidePrelati <davide.prelati@gmail.com>
2024-06-21 11:46:56 +0200

    first import

---
+++ GestionePrenotazioni/src/test/java/davide_prelati/GestionePrenotazioni/GestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package davide_prelati.GestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
